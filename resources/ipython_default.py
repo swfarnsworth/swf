345b2a5df81fa7e9e4db65bb6533f87f45e3ed62
Steele Farnsworth <swfarnsworth@gmail.com>
2024-11-10 07:24:42 -0500

    change ipython default to display numbers with underscore separators

--- resources/ipython_default.py
+++ resources/ipython_default.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+
+ip = get_ipython()
+# display numbers with underscore separators
+for cls in [int, float]:
+    ip.display_formatter.formatters['text/plain'].for_type(cls, lambda number, printer, cycle: printer.text(f'{number:_}'))
